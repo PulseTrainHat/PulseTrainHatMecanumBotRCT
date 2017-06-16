eeeec067ced60ba41e9ee1e202b509c097850fe8
PTHAT <info@pthat.com>
2017-06-16 15:05:10 +0100

    Add files via upload

---
+++ PulseTrainHatMecanumBotRCT-V3/PulseTrainHatMecanumRCT/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulseTrainHatMecanumRCT
+{
+    
+    public class Descriptors
+    {
+        public const UInt16 vendorId = 0x1781;
+        public const UInt16 productId = 0x85C;
+        public const UInt16 usagePage = 0xFF00;
+        public const UInt16 usageId = 0x0001;
+        public const string ProductString = "PTHAT RC";
+    }
+}
+
